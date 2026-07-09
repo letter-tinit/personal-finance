a294043650c47371f97bb3675aec44f68ec7ea55
letter-tinit <letter.tinit@gmail.com>
2026-07-09 15:27:09 +0700

    Initial Commit

---
+++ Personal Finance/Personal_FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_FinanceApp.swift
+//  Personal Finance
+//
+//  Created by TiniT on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Personal_FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
